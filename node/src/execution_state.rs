197c476146d28dd978fb543605cf320a575c2506
Owen Colegrove <owen@algofi.org>
2022-07-27 23:23:42 -0400

    add FullExecution which consumes txn variant

--- node/src/execution_state.rs
+++ node/src/execution_state.rs
@@ -6,6 +6,8 @@ use consensus::ConsensusOutput;
 use crypto::traits::VerifyingKey;
 use executor::{ExecutionIndices, ExecutionState, ExecutionStateError};
 use thiserror::Error;
+use types::{TransactionRequest, TransactionVariant};
+
 
 /// A simple/dumb execution engine.
 pub struct SimpleExecutionState;
@@ -13,6 +15,36 @@ pub struct SimpleExecutionState;
 #[async_trait]
 impl ExecutionState for SimpleExecutionState {
     type Transaction = String;
+    type Error = SimpleExecutionError;
+    type Outcome = Vec<u8>;
+
+    async fn handle_consensus_transaction<PublicKey: VerifyingKey>(
+        &self,
+        _consensus_output: &ConsensusOutput<PublicKey>,
+        _execution_indices: ExecutionIndices,
+        _transaction: Self::Transaction,
+    ) -> Result<(Self::Outcome, Option<Committee<PublicKey>>), Self::Error> {
+        Ok((Vec::default(), None))
+    }
+
+    fn ask_consensus_write_lock(&self) -> bool {
+        true
+    }
+
+    fn release_consensus_write_lock(&self) {}
+
+    async fn load_execution_indices(&self) -> Result<ExecutionIndices, Self::Error> {
+        Ok(ExecutionIndices::default())
+    }
+}
+
+
+/// Onboarding transaction logic into execution state
+pub struct FullExecutionState;
+
+#[async_trait]
+impl ExecutionState for FullExecutionState {
+    type Transaction = TransactionRequest<TransactionVariant>;
     type Error = SimpleExecutionError;
     type Outcome = Vec<u8>;
 
