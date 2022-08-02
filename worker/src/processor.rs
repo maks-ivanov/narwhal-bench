049c267b21dfa57417602d0461355b0e924316d2
Owen Colegrove <owen@algofi.org>
2022-08-02 09:55:08 -0400

    add deserialization to received batches

--- worker/src/processor.rs
+++ worker/src/processor.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Facebook, Inc. and its affiliates
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-
+use crate::worker::TxReceiverHandler;
 use config::WorkerId;
 use crypto::traits::VerifyingKey;
 use store::Store;
@@ -14,8 +14,8 @@ use tokio::{
 };
 use tracing::error;
 use types::{
-    error::DagError, serialized_batch_digest, BatchDigest, ReconfigureNotification,
-    SerializedBatchMessage, WorkerPrimaryMessage,
+    error::DagError, serialized_batch_digest, Batch, BatchDigest, ReconfigureNotification,
+    SerializedBatchMessage, WorkerMessage, WorkerPrimaryMessage, SERIALIZED_TRANSACTION_LENGTH,
 };
 
 #[cfg(test)]
@@ -44,6 +44,35 @@ impl Processor {
             loop {
                 tokio::select! {
                     Some(batch) = rx_batch.recv() => {
+                        // TODO - note how redundant the two lines below are, ~~ we can do much better ~~
+
+                        // check that we are able to correctly deserialize the batch
+                        // this is one deserialization method, leveraging my newly defined deserialize batch
+                        // let deserialized_batch = deserialize_batch(&batch);
+                        // match deserialized_batch {
+                        //     Ok(_) => {}
+                        //     Err(error) => {
+                        //         error!("Received invalid batch, serialization failure: {error}");
+                        //     }
+                        // }
+
+                        // let types::WorkerMessage::<PublicKey>::Batch(deserialized_batch) = bincode::deserialize(&batch.to_vec()).unwrap();
+
+                        // check that we are able to correctly deserialize the batch and validate signatures
+                        // this is another deserialization method, matching patterns observed throughout Narwhal
+                        match bincode::deserialize(&batch).unwrap() {
+                            WorkerMessage::<PublicKey>::Batch(Batch(transactions)) => {
+                                for transaction_padded in transactions {
+                                    let transaction: Vec<u8> = transaction_padded.to_vec()
+                                                                            .drain(..SERIALIZED_TRANSACTION_LENGTH)
+                                                                            .collect();
+
+                                    TxReceiverHandler::verify_incoming_transaction(transaction).unwrap();
+                                }
+                            },
+                            _ => panic!("gg"),
+                        };
+
                         // Hash the batch.
                         let res_digest = serialized_batch_digest(&batch);
 
